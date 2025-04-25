806c6de52df5cd4be05ff991055cd5a62d34963a
shery1-11 <slee92@risd.edu>
2025-04-25 16:23:19 -0400

    dgfghfgxrf

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 
 let img;
+let c1,c2;
 let x=0;
 // Mouse to draw!
 
@@ -27,7 +28,14 @@ function draw() {
 }
   
 function mousePressed(){
-    background(255);
+    c1 = color(255);
+    c2 = color(63, 191, 191);
+    for(let y=0; y<height; y++){
+        n = map(y,0,height,0,1);
+        let newc = lerpColor(c1,c2,n);
+        stroke(newc);
+        line(0,y,width, y);
+      }
       x=50;
 }
 function windowResized(){
